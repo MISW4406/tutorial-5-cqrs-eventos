6037b01582ef321b2d9bc43368452f61678c81f4
HUMBERTO ENRIQUE MAURY MAURY <h.maury@uniandes.edu.co>
2025-02-26 15:44:11 -0500

    actualizar la variable de ambiente para ser consistente con la definida en el docker-compose

--- src/aeroalpes/seedwork/infraestructura/utils.py
+++ src/aeroalpes/seedwork/infraestructura/utils.py
@@ -5,5 +5,5 @@ def time_millis():
     return int(time.time() * 1000)
 
 def broker_host():
-    return os.getenv('BROKER_HOST', default="localhost")
+    return os.getenv('PULSAR_ADDRESS', default="localhost")
 
